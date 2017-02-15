b5653342822b5c5693b25f1f6748909344f0cd26
peter2zou <peter2zou@gmail.com>
2017-02-15 08:01:14 -0500

    Update GridCoordinate.cs

--- RobotPosition.Entity/GridCoordinate.cs
+++ RobotPosition.Entity/GridCoordinate.cs
@@ -1,11 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
 namespace RobotPosition.Entity
 {
     public class GridCoordinate:IGridCoordinate
     {
-        [MaxLength(50)]
         public int MaxXCoordinate {set;get;}
-        [MaxLength(50)]
         public int MaxYCoordinate {set;get;}
     }
 }
