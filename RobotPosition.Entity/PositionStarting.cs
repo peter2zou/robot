0e40c59d589814155ae311ee9628560d208c3608
peter2zou <peter2zou@gmail.com>
2017-02-15 08:03:01 -0500

    Update PositionStarting.cs

--- RobotPosition.Entity/PositionStarting.cs
+++ RobotPosition.Entity/PositionStarting.cs
@@ -1,14 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
 namespace RobotPosition.Entity
 {
     public class PositionStarting: IPositionStarting
     {
-        [MaxLength(50)]
         public int StartingXPosition {get;set;}
-        [MaxLength(50)]
         public int StartingYPosition {get;set;}
         public char Orientation {get;set;}
-        [MaxLength(15)]
         public string Instructions {get;set;}
     }
 }
